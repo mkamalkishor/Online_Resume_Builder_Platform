59474c7cda3f6e4f08afcd5ecaf10997a36693c3
mkamalkishor <144911821+mkamalkishor@users.noreply.github.com>
2026-01-19 17:39:19 +0530

    Add Gunicorn command for WSGI application
    Added Gunicorn command to run the WSGI application.

--- Online_Resume_Builder_Platform/wsgi.py
+++ Online_Resume_Builder_Platform/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Online_Resume_Builder_Platform.settings')
 
 application = get_wsgi_application()
+
+gunicorn Online_Resume_Builder_Platform.wsgi:application
